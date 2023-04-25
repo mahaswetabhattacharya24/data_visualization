667ecce659030f9eb6ff6f293e277ba72d1276e6
Mahasweta Bhattacharya <mbhatt17@jh.edu>
2023-04-25 09:09:02 -0400

    Minute changes in the parameter specification

--- dose_profiles.R
+++ dose_profiles.R
@@ -15,9 +15,9 @@ colnames(dose_profile_data) <- c('Depth', 'TPS_IDD', 'Python_IDD','TPS_Sigma',
 
 #Plot
 ggplot(data = dose_profile_data) +
-  geom_line(aes(x = Depth, y = TPS_IDD, color = 'blue'), linewidth = 2) +
-  geom_line(aes(x = Depth, y = Python_IDD, color = 'red'), linetype = 'dashed',linewidth = 2) +
-  scale_color_discrete(name="", labels=c('TPS', 'Python Dose Engine')) +
-  ylab('Integrated depth dose profile')+
+  geom_line(aes(x = Depth, y = TPS_IDD, color = "blue"), linewidth = 2) +
+  geom_line(aes(x = Depth, y = Python_IDD, color = "red"), linetype = "dashed",linewidth = 2) +
+  scale_color_discrete(name="", labels=c("TPS", "Python Dose Engine")) +
+  ylab("Integrated depth dose profile")+
   ggtitle("99.8 MeV")
 
