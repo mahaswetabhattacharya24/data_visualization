b1c4a8da53de7ec3db7faa4006fd0de6cc875a57
Mahasweta Bhattacharya <mbhatt17@jh.edu>
2023-04-25 14:29:06 -0400

    updates to the plot themes,addition of font type, color specification, font size

--- dose_profiles.R
+++ dose_profiles.R
@@ -17,11 +17,18 @@ colnames(dose_profile_data) <- c('Depth', 'TPS_IDD', 'Python_IDD','TPS_Sigma',
 
 #Plot
 ggplot(data = dose_profile_data) +
-  geom_line(aes(x = Depth, y = TPS_IDD, color = "blue"), linewidth = 2) +
-  geom_line(aes(x = Depth, y = Python_IDD, color = "red"), linetype = "dashed",linewidth = 2) +
-  scale_color_discrete(name="", labels=c("TPS", "Python Dose Engine")) +
+  geom_line(aes(x = Depth, y = TPS_IDD, color = "TPS"), linewidth = 3) +
+  geom_line(aes(x = Depth, y = Python_IDD, color = "Python Dose Engine"), linetype = "dashed",linewidth = 3) +
+  scale_color_manual(name="", values=c("TPS"="#0000ff", "Python Dose Engine"="#ff3300")) +
   ylab("Integrated depth dose profile")+
-  ggtitle("99.8 MeV")
+  ggtitle("99.8 MeV")+
+  theme(plot.title = element_text(hjust = 0.5, size = 24, face = "bold"), 
+        axis.title.x = element_text(size = 18, face = "bold"), 
+        axis.title.y = element_text(size = 18, face = "bold"),
+        axis.text.x = element_text(size = 14, face = "bold"), 
+        axis.text.y = element_text(size = 14, face = "bold"),
+        legend.text = element_text(size = 14))
+ggsave("99.8MeV_water.tiff", dpi=300)
 
 
 ## 130.2 MeV
@@ -35,11 +42,18 @@ colnames(dose_profile_data) <- c('Depth', 'TPS_IDD', 'Python_IDD','TPS_Sigma',
 
 #Plot
 ggplot(data = dose_profile_data) +
-  geom_line(aes(x = Depth, y = TPS_IDD, color = "blue"), linewidth = 2) +
-  geom_line(aes(x = Depth, y = Python_IDD, color = "red"), linetype = "dashed",linewidth = 2) +
-  scale_color_discrete(name="", labels=c("TPS", "Python Dose Engine")) +
+  geom_line(aes(x = Depth, y = TPS_IDD, color = "TPS"), linewidth = 3) +
+  geom_line(aes(x = Depth, y = Python_IDD, color = "Python Dose Engine"), linetype = "dashed",linewidth = 3) +
+  scale_color_manual(name="", values=c("TPS"="#0000ff", "Python Dose Engine"="#ff3300")) +
   ylab("Integrated depth dose profile")+
-  ggtitle("130.2 MeV")
+  ggtitle("130.2 MeV")+
+  theme(plot.title = element_text(hjust = 0.5, size = 24, face = "bold"), 
+        axis.title.x = element_text(size = 18, face = "bold"), 
+        axis.title.y = element_text(size = 18, face = "bold"),
+        axis.text.x = element_text(size = 14, face = "bold"), 
+        axis.text.y = element_text(size = 14, face = "bold"))
+ggsave("130.2MeV_water.tiff", dpi=300)
+
 
 ## 228.7 MeV
 data_file <- "228.7MeV_validation_table_water.csv"
@@ -52,9 +66,16 @@ colnames(dose_profile_data) <- c('Depth', 'TPS_IDD', 'Python_IDD','TPS_Sigma',
 
 #Plot
 ggplot(data = dose_profile_data) +
-  geom_line(aes(x = Depth, y = TPS_IDD, color = "blue"), linewidth = 2) +
-  geom_line(aes(x = Depth, y = Python_IDD, color = "red"), linetype = "dashed",linewidth = 2) +
-  scale_color_discrete(name="", labels=c("TPS", "Python Dose Engine")) +
+  geom_line(aes(x = Depth, y = TPS_IDD, color = "TPS"), linewidth = 3) +
+  geom_line(aes(x = Depth, y = Python_IDD, color = "Python Dose Engine"), linetype = "dashed",linewidth = 3) +
+  scale_color_manual(name="", values=c("TPS"="#0000ff", "Python Dose Engine"="#ff3300")) +
   ylab("Integrated depth dose profile")+
-  ggtitle("228.7 MeV")
+  ggtitle("228.7 MeV")+
+  theme(plot.title = element_text(hjust = 0.5, size = 24, face = "bold"), 
+        axis.title.x = element_text(size = 18, face = "bold"), 
+        axis.title.y = element_text(size = 18, face = "bold"),
+        axis.text.x = element_text(size = 14, face = "bold"), 
+        axis.text.y = element_text(size = 14, face = "bold"))
+ggsave("228.7MeV_water.tiff", dpi=300)
+
 
